221e3e11c65d606d8473d3fe3c52a1d4cda248d7
Priyansh #CrazyBoy <106391059+PriyanshGoswami@users.noreply.github.com>
2023-04-16 20:37:07 +0530

    Update AIrobot.py

--- AIrobot.py
+++ AIrobot.py
@@ -2,14 +2,12 @@ import tensorflow as tf
 import telebot
 from telebot import types
 import pyrogram
-
-bot = telebot.TeleBot("YOUR_BOT_TOKEN")
+from Rengoku import pbot
 
 # Load the pre-trained GAN model
 model = tf.keras.models.load_model("gan_model.h5")
 
-@bot.message_handler(commands=['start', 'generate'])
-def generate_image("Hello \n I M AI Image Generator Bot \n\n Commands :- /start , /generate"):
+@pbot.message_handler(commands=['generate'])
     # Generate a random noise vector
     noise = tf.random.normal((1, 100))
     # Use the GAN to generate an image
@@ -18,9 +16,7 @@ def generate_image("Hello \n I M AI Image Generator Bot \n\n Commands :- /start 
     generated_image = generated_image.reshape(28, 28, 1)
     # Convert the image to a PNG file
     img_file = tf.keras.preprocessing.image.array_to_img(generated_image)
-    img_file.save("@emAIImageGenerator_Bot Photo.png")
+    img_file.save("@KyojuroRengokuRobot.png")
     # Send the generated image to the user
-    with open("@emAIImageGenerator_Bot Photo.png", "rb") as image:
+    with open("@KyojuroRengokuRobot Photo.png", "rb") as image:
         bot.send_photo(message.chat.id, image)
-
-bot.polling()
